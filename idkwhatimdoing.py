ecb56dee779e7511aa2eccd4ce35cbd9972b8150
darianabhulimen <darianabhulimen@ufl.edu>
2024-04-10 11:17:39 -0400

    added main and encoder

--- idkwhatimdoing.py
+++ idkwhatimdoing.py
@@ -1,26 +1,30 @@
-while True:
-    print('Menu')
-    print('-------------')
-    print('1. Encode')
-    print('2. Decode')
-    print('3. Quit')
-    print('')
-    a = input('Please enter an option: ')
-    if a.isdigit():
-        a = int(a)
+def menu():
+    while True:
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        print('')
+        a = input('Please enter an option: ')
+        if a.isdigit():
+            a = int(a)
 
-    if a == 1:
-        uinput = input('Please enter your password to encode: ')
-        encode = []
-        for i in range(len(uinput)):
-            ndigit = int(uinput[i]) + 3
-            encode.append(str(ndigit))
+        if a == 1:
+            uinput = input('Please enter your password to encode: ')
+            encode = []
+            for i in range(len(uinput)):
+                ndigit = int(uinput[i]) + 3
+                encode.append(str(ndigit))
 
-        final_encode = ''.join(encode)
-        print('Your password has been encoded and stored!')
+            final_encode = ''.join(encode)
+            print('Your password has been encoded and stored!')
 
-    elif a == 2:
-        print(f'The encoded password is {final_encode}, and the original password is {uinput}.')
+        elif a == 2:
+            print(f'The encoded password is {final_encode}, and the original password is {uinput}.')
 
-    else:
-        break
+        else:
+            break
+
+if __init__ == "__main__":
+    menu()
