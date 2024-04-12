ccad20f137e5641bd3fce3e2ba1e2d6c571d562c
mansapatel111 <142267357+mansapatel111@users.noreply.github.com>
2024-04-12 16:53:57 -0400

    Update idkwhatimdoing.py
    added decoder and fixed some the print statement for decoder - mansa

--- idkwhatimdoing.py
+++ idkwhatimdoing.py
@@ -1,30 +1,44 @@
-def encode():
+def encode(uinput):
+    encode = []
+    for i in range(len(uinput)):
+        ndigit = int(uinput[i]) + 3
+        encode.append(str(ndigit))
+
+        final_encode = ''.join(encode)
+    return final_encode
+
+def decode(final_encode):
+    decoded = ""
+    for i in final_encode:
+        i = int(i)
+        i -= 3
+        i = str(i)
+        decoded += i
+    return decoded
+
+def main():
     while True:
-        print('Menu')
-        print('-------------')
-        print('1. Encode')
-        print('2. Decode')
-        print('3. Quit')
-        print('')
-        a = input('Please enter an option: ')
-        if a.isdigit():
-            a = int(a)
+            print('Menu')
+            print('-------------')
+            print('1. Encode')
+            print('2. Decode')
+            print('3. Quit')
+            print('')
+            a = input('Please enter an option: ')
+            if a.isdigit():
+                a = int(a)
 
-        if a == 1:
-            uinput = input('Please enter your password to encode: ')
-            encode = []
-            for i in range(len(uinput)):
-                ndigit = int(uinput[i]) + 3
-                encode.append(str(ndigit))
+            if a == 1:
+                uinput = input('Please enter your password to encode: ')
+                encoded = encode(uinput)
+                print('Your password has been encoded and stored!')
 
-            final_encode = ''.join(encode)
-            print('Your password has been encoded and stored!')
+            elif a == 2:
+                decoded = decode(encoded)
+                print(f'The encoded password is {encoded}, and the original password is {decoded}.')
 
-        elif a == 2:
-            print(f'The encoded password is {final_encode}, and the original password is {uinput}.')
-
-        else:
-            break
+            else:
+                break
 
 if __name__ == "__main__":
-    encode()
+    main()
